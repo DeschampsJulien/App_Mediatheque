a8d30e16e7b964200790841b8ac483cf1fb9264c
Julien Deschamps <jdeschamps34@yahoo.fr>
2024-06-21 14:21:48 +0200

    Delete Media

--- mediatheque/bibliothecaire/models.py
+++ mediatheque/bibliothecaire/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
+
 class Media(models.Model):
     title = models.CharField('Titre', max_length=200)
     available = models.BooleanField('Disponible', default=True)
-
-    def __str__(self):
-        return self.title
 
     class Meta:
         abstract = True
@@ -15,7 +13,8 @@ class Book(Media):
     author = models.CharField( 'Auteur', max_length=150)
     
     class Meta:
-        verbose_name = "livre"
+        verbose_name = "Livre"
+
 
 class Dvd(Media):
     realizator = models.CharField( 'Réalisateur', max_length=150)
@@ -23,19 +22,54 @@ class Dvd(Media):
     class Meta:
         verbose_name = "DVD"
 
+
 class Cd(Media):
     artist = models.CharField('Artiste', max_length=150)
     
     class Meta:
         verbose_name = "CD"
 
+
 class BoardGame(models.Model):
     name = models.CharField('Nom', max_length=200)
     creator = models.CharField('Créateur', max_length=150)
-    sur_place = models.BooleanField('Disponible', default=True)
-    
-    def __str__(self):
-        return self.name
+    available_on_site = models.BooleanField('Disponible', default=True)
     
     class Meta:
         verbose_name = "Jeu de Plateau"
+
+
+class Rented(models.Model):
+    
+    class Status(models.TextChoices):
+        rented = 'Loué',
+        available = 'Disponible'
+    
+    date_rented = models.DateField(auto_now=True)
+    date_restitution = models.DateField(null=True)
+    rented = models.BooleanField(default=False)
+    status = models.CharField(choices=Status.choices,max_length=15)
+    
+    class Meta:
+        abstract = True
+
+
+class RentedBook(Rented):
+    book = models.OneToOneField(Book, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "Emprunt Livre"
+
+
+class RentedDvd(Rented):
+    dvd = models.OneToOneField(Dvd, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "Emprunt DVD"
+
+
+class RentedCd(Rented):
+    cd = models.OneToOneField(Cd, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "Emprunt CD"
--- mediatheque/bibliothecaire/views.py
+++ mediatheque/bibliothecaire/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from bibliothecaire.models import Book, Dvd, Cd, BoardGame, Media
-from bibliothecaire.forms import BookForm, DvdForm, CdForm, BoardGameForm, RentedMedia
+from bibliothecaire.models import Book, Dvd, Cd, BoardGame
+from bibliothecaire.forms import BookForm, DvdForm, CdForm, BoardGameForm, RentedBookForm
 
 
 # AFFICHAGE DE LA LISTE DES MEDIAS APP_BIBLIOTHECAIRE
@@ -15,7 +15,7 @@ def list_medias_bibliothecaire(request):
         'books': books,
         'dvds': dvds,
         'cds' : cds,
-        'boardgames' : boardgames
+        'boardgames' : boardgames,
         })
 
 
@@ -30,7 +30,7 @@ def list_medias_member(request):
         'books': books,
         'dvds': dvds,
         'cds' : cds,
-        'boardgames' : boardgames
+        'boardgames' : boardgames,
         })
 
 
@@ -40,11 +40,28 @@ def ajout_book(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
+
         return HttpResponseRedirect('/bibliothecaire')
+    
     else:
         form = BookForm()
+
     return render(request,"bibliothecaire/ajout_book.html", {'form': form})
 
+
+# SUPPRIMER UN LIVRE
+# DEUX VERSION DE REDIRECTION
+# def delete_book(request, id):
+#     book = Book.objects.get(pk=id)
+#     book.delete()
+#     books = Book.objects.all()
+
+#     return render(request, 'bibliothecaire/list_medias_bibliothecaire.html', {'books': books})
+
+def delete_book(request, id):
+    book = Book.objects.get(pk=id)
+    book.delete()
+    return HttpResponseRedirect('/bibliothecaire')
 
 # CREATION D'UN NOUVEAU DVD
 def ajout_dvd(request):
@@ -52,10 +69,20 @@ def ajout_dvd(request):
         form = DvdForm(request.POST)
         if form.is_valid():
             form.save()
+
         return HttpResponseRedirect('/bibliothecaire')
+    
     else:
         form = DvdForm()
+
     return render(request,"bibliothecaire/ajout_dvd.html", {'form': form})
+
+
+# SUPPRIMER UN DVD
+def delete_dvd(request, id):
+    dvd = Dvd.objects.get(pk=id)
+    dvd.delete()
+    return HttpResponseRedirect('/bibliothecaire')
 
 
 # CREATION D'UN NOUVEAU CD
@@ -64,10 +91,20 @@ def ajout_cd(request):
         form = CdForm(request.POST)
         if form.is_valid():
             form.save()
+
         return HttpResponseRedirect('/bibliothecaire')
+    
     else:
         form = CdForm()
+
     return render(request,"bibliothecaire/ajout_cd.html", {'form': form})
+
+
+# SUPPRIMER UN CD
+def delete_cd(request, id):
+    cd = Cd.objects.get(pk=id)
+    cd.delete()
+    return HttpResponseRedirect('/bibliothecaire')
 
 
 # CREATION D'UN NOUVEAU JEU DE PLATEAU
@@ -76,38 +113,35 @@ def ajout_boardgame(request):
         form = BoardGameForm(request.POST)
         if form.is_valid():
             form.save()
+
         return HttpResponseRedirect('/bibliothecaire')
+    
     else:
         form = BoardGameForm()
     
     return render(request,"bibliothecaire/ajout_boardgame.html", {'form': form})
 
-# LOUER UN MEDIA
-def rented_media(request):
-    books = Book.objects.all()
-    dvds = Dvd.objects.all()
-    cds = Cd.objects.all()
-    boardgames = BoardGame.objects.all()
 
-    return render(request, "bibliothecaire/rented_media.html", {
-        'books': books,
-        'dvds': dvds,
-        'cds' : cds,
-        'boardgames' : boardgames
-        })
+# SUPPRIMER UN BOARDGAME
+def delete_boardgame(request, id):
+    boardgame = BoardGame.objects.get(pk=id)
+    boardgame.delete()
+    return HttpResponseRedirect('/bibliothecaire')
+
+
+# LOUER UN BOOK
+def rented_book(request):
+    if request.method == 'POST':
+        form = RentedBookForm(request.POST)
+        if form.is_valid():
+            rentedbook = form.save()
+            book = rentedbook.book
+            book.available = False
+            book.save()
+
+        return HttpResponseRedirect('/bibliothecaire')
     
-# def rented_media(request, id):
-#     if request.method == 'POST':
-#         media = Media.objects.get(pk=id)
-#         rentedmedia = RentedMedia(request.POST)
-#         if rentedmedia.is_valid():
-#             media.title = rentedmedia.cleaned_data['title']
-#             media.available = rentedmedia.cleaned_data['available']
-#             media.save()
-#             medias = Media.objects.all()
-#             return render(request,'bibliothecaire/list_medias_member.html',
-#                         {'medias': medias})
-#     else:
-#         rentedmedia = RentedMedia()
-#         return render(request,'bibliothecaire/rented_media.html',
-#                     {'rentedmedia': rentedmedia})
+    else:
+        form = RentedBookForm()
+    
+    return render(request,"bibliothecaire/rented_media.html", {'form': form})
